1846bdd8070fcb6b69fb959111dc2c02225ac3fb
ajlucena78 <ajlucena78@gmail.com>
2017-12-19 12:06:05 +0100

    FastTraverse return parents with a key and value given

--- lib/Ximdex/Models/FastTraverse.php
+++ lib/Ximdex/Models/FastTraverse.php
@@ -1,76 +1,107 @@
 <?php
-    namespace Ximdex\Models;
-    
-    use Ximdex\Models\ORM\FastTraverseOrm;
-    use Ximdex\Runtime\Db;
-    
-    class FastTraverse extends FastTraverseOrm
+
+namespace Ximdex\Models;
+
+use Ximdex\Logger;
+use Ximdex\Models\ORM\FastTraverseOrm;
+use Ximdex\Runtime\Db;
+
+class FastTraverse extends FastTraverseOrm
+{
+    /**
+     * Getting an array with all the child nodes which depend of a parent node given
+     * This array contains the IdNode field with the Depth value in its index
+     * If the parameter nodeTypes is a true value, the index of each node will be its ID and the value the Node Type ID
+     * @param int $idNode
+     * @param bool $nodeTypes
+     * @param int $level
+     * @return boolean|array
+     */
+    public static function get_children(int $idNode, bool $nodeTypes = false, int $level = null)
     {
-        /**
-         * Getting an array with all the child nodes which depend of a parent node given
-         * This array contains the IdNode field with the Depth value in its index
-         * If the parameter nodeTypes is a true value, the index of each node will be its ID and the value the Node Type ID
-         * @param int $idNode
-         * @param bool $nodeTypes
-         * @param int $level
-         * @return bool|string[]
-         */
-        function getChildren(int $idNode, bool $nodeTypes = false, int $level = null)
+        if ($idNode < 1)
         {
-            if ($idNode < 1)
-                return false;
-            $db = new Db();
-            $sql = 'select ft.IdChild, ft.Depth';
-            if ($nodeTypes)
-                $sql .= ', node.IdNodeType';
-            $sql .= ' from FastTraverse ft';
-            if ($nodeTypes)
-                $sql .= ' inner join Nodes node on (node.IdNode = ft.IdChild)';
-            $sql .= ' where ft.IdNode = ' . $idNode;
-            if ($level)
-                $sql .= ' and ft.Depth = ' . $level;
-            if ($db->Query($sql) === false)
-                return false;
-            $children = array();
-            if ($nodeTypes)
-            {
-                while (!$db->EOF)
-                {
-                    $children[$db->GetValue('Depth')][$db->GetValue('IdChild')] = $db->GetValue('IdNodeType');
-                    $db->Next();
-                }
-            }
-            else
-            {
-                while (!$db->EOF)
-                {
-                    $children[$db->GetValue('Depth')][] = $db->GetValue('IdChild');
-                    $db->Next();
-                }
-            }
-            return $children;
+            Logger::error('Getting children in FastTraverse without node given');
+            return false;
         }
-    
-        /**
-         * Getting an array with all the parent nodes which depend of a child node given
-         * This array contains the IdNode field with the Depth value in its index
-         * @param int $idChildren
-         * @return bool|string[]
-         */
-        function getParents(int $idNode)
+        $db = new Db();
+        $sql = 'select ft.IdChild, ft.Depth';
+        if ($nodeTypes)
+            $sql .= ', node.IdNodeType';
+        $sql .= ' from FastTraverse ft';
+        if ($nodeTypes)
+            $sql .= ' inner join Nodes node on (node.IdNode = ft.IdChild)';
+        $sql .= ' where ft.IdNode = ' . $idNode;
+        if ($level)
+            $sql .= ' and ft.Depth = ' . $level;
+        $sql .= ' order by ft.Depth';
+        if ($db->Query($sql) === false)
         {
-            if ($idNode < 1)
-                return false;
-            $db = new Db();
-            $sql = 'select IdNode, Depth from FastTraverse where IdChild = ' . $idNode;
-            if ($db->Query($sql) === false)
-                return false;
-            $parents = array();
+            Logger::error('Getting children in FastTraverse with node: ' . $idNode . ' (' . $db->desErr . ')');
+            return false;
+        }
+        $children = array();
+        if ($nodeTypes)
+        {
             while (!$db->EOF)
             {
-                $parents[$db->GetValue('Depth')][] = $db->GetValue('IdNode');
+                $children[$db->GetValue('Depth')][$db->GetValue('IdChild')] = $db->GetValue('IdNodeType');
                 $db->Next();
             }
-            return $parents;
         }
+        else
+        {
+            while (!$db->EOF)
+            {
+                $children[$db->GetValue('Depth')][] = $db->GetValue('IdChild');
+                $db->Next();
+            }
+        }
+        return $children;
     }
+
+    /**
+     * Getting an array with all the parent nodes which depend of a child node given
+     * This array contains the IdNode field with the Depth value in its index
+     * @param int $idNode
+     * @param string $index
+     * @return boolean|array
+     */
+    public static function get_parents(int $idNode, string $value = null, string $index = null)
+    {
+        if ($idNode < 1)
+        {
+            Logger::error('Getting parents in FastTraverse without node given');
+            return false;
+        }
+        if ($index or $value)
+        {
+            if (!$index)
+                $index = 'IdNode';
+            if (!$value)
+                $value = 'Depth';
+            $sql = 'select node.' . $value . ', node.' . $index . ' from FastTraverse ft';
+            $sql .= ' inner join Nodes node on (node.IdNode = ft.IdNode)';
+        }
+        else
+        {
+            $index = 'IdNode';
+            $value = 'Depth';
+            $sql = 'select ft.' . $index . ', ft.' . $value . ' from FastTraverse ft';
+        }
+        $sql .= ' where ft.IdChild = ' . $idNode . ' order by ft.Depth';
+        $db = new Db();
+        if ($db->Query($sql) === false)
+        {
+            Logger::error('Getting parents in FastTraverse with node: ' . $idNode . ' (' . $db->desErr . ')');
+            return false;
+        }
+        $parents = array();
+        while (!$db->EOF)
+        {
+            $parents[$db->GetValue($index)] = $db->GetValue($value);
+            $db->Next();
+        }
+        return $parents;
+    }
+}
